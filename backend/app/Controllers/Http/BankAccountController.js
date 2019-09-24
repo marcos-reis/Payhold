a35f339c43c87fc104a287dd7c9de6b08fcc2fac
Marcos-Reis <quinhopt@gmail.com>
2019-09-24 07:56:07 -0300

    default REST in the bankaccount

--- backend/app/Controllers/Http/BankAccountController.js
+++ backend/app/Controllers/Http/BankAccountController.js
@@ -2,27 +2,45 @@
 const BankAccount = use("App/Models/BankAccount");
 const User = use("App/Models/User");
 class BankAccountController {
-  async store({ request }) {
-    const { cod, bank, agency, account, user_id } = request.all();
-    const bankaccount = await BankAccount.create({
-      cod,
-      bank,
-      agency,
-      account,
-      user_id
-    });
-    return { bankaccount };
-  }
-  async show({ request }) {
-    const { id } = request.params;
+	async store({ request, response }) {
+		const { cod, bank, agency, account, user_id } = request.all();
 
-    const bankaccounts = await User.query()
-      .select("id", "fullname")
-      .where("id", id)
-      .with("accounts")
-      .fetch();
-    return { bankaccounts };
-  }
+		if (!cod || !bank || !agency || !account || !user_id) {
+			return response.status(401).json({ mensage: "Enter valid data user." });
+		}
+		const listbankaccountfetch = await BankAccount.query()
+			.where("user_id", user_id)
+			.where("account", account)
+			.fetch();
+		const listbankaccount = listbankaccountfetch.toJSON();
+
+		if (!!listbankaccount[0]) {
+			if (listbankaccount[0].account) {
+				return response
+					.status(401)
+					.json({ message: "Bankaccount already exists." });
+			}
+		}
+
+		const bankaccount = await BankAccount.create({
+			cod,
+			bank,
+			agency,
+			account,
+			user_id
+		});
+		return response.status(200).json({ bankaccount });
+	}
+	async show({ request, response }) {
+		const { id } = request.params;
+
+		const bankaccounts = await User.query()
+			.select("id", "fullname")
+			.where("id", id)
+			.with("accounts")
+			.fetch();
+		return response.status(200).json({ bankaccounts });
+	}
 }
 
 module.exports = BankAccountController;
--- backend/test/functional/bankaccount.spec.js
+++ backend/test/functional/bankaccount.spec.js
@@ -8,62 +8,95 @@ const BankAccount = use("App/Models/BankAccount");
 trait("Test/ApiClient");
 
 suite.beforeEach(async () => {
-  await ace.call("migration:refresh", {}, { silent: true });
+	await ace.call("migration:refresh", {}, { silent: true });
 });
 
 test("It should register a new user account", async ({ assert, client }) => {
-  const user = await User.create({
-    fullname: "Marcos Reis dos Santos",
-    shortname: "Marcos Reis",
-    email: "marcosreisdossantos01@gmail.com",
-    cpf: "00011122233",
-    saldo: "2.574.900,00",
-    rendimento: "1500,00",
-    extrato: "Em breve",
-    password: "abc123"
-  });
-  const response = await client
-    .post("/bankaccount")
-    .send({
-      cod: "060",
-      bank: "Nubank",
-      account: "123456",
-      agency: "1234",
-      user_id: user.id
-    })
-    .end();
-  response.assertStatus(200);
-  assert.exists(response.body.bankaccount);
+	const user = await User.create({
+		fullname: "Marcos Reis dos Santos",
+		shortname: "Marcos Reis",
+		email: "marcosreisdossantos01@gmail.com",
+		cpf: "00011122233",
+		saldo: "2.574.900,00",
+		rendimento: "1500,00",
+		extrato: "Em breve",
+		password: "abc123"
+	});
+	const response = await client
+		.post("/bankaccount")
+		.send({
+			cod: "060",
+			bank: "Nubank",
+			account: "123456",
+			agency: "1234",
+			user_id: user.id
+		})
+		.end();
+	response.assertStatus(200);
+	assert.exists(response.body.bankaccount);
+});
+
+test("It not should register a new user account with already exists data", async ({
+	assert,
+	client
+}) => {
+	await BankAccount.create({
+		cod: "060",
+		bank: "Nubank",
+		account: "123456",
+		agency: "1234",
+		user_id: 1
+	});
+
+	const response = await client
+		.post("/bankaccount")
+		.send({
+			cod: "060",
+			bank: "Nubank",
+			account: "123456",
+			agency: "1234",
+			user_id: 1
+		})
+		.end();
+
+	response.assertStatus(401);
+});
+
+test("It not should register a new user account with invalid data", async ({
+	client
+}) => {
+	const response = await client.post("/bankaccount").end();
+	response.assertStatus(401);
 });
 
 test("It should list all account of a user", async ({ assert, client }) => {
-  const user = await User.create({
-    fullname: "Marcos Reis dos Santos",
-    shortname: "Marcos Reis",
-    email: "marcosreisdossantos01@gmail.com",
-    cpf: "00011122233",
-    saldo: "2.574.900,00",
-    rendimento: "1500,00",
-    extrato: "Em breve",
-    password: "abc123"
-  });
-  await BankAccount.create({
-    cod: "060",
-    bank: "Nubank",
-    account: "123456",
-    agency: "0001",
-    user_id: user.id
-  });
-  await BankAccount.create({
-    cod: "237",
-    bank: "Bradesco",
-    account: "123459",
-    agency: "1234",
-    user_id: user.id
-  });
+	const user = await User.create({
+		fullname: "Marcos Reis dos Santos",
+		shortname: "Marcos Reis",
+		email: "marcosreisdossantos01@gmail.com",
+		cpf: "00011122233",
+		saldo: "2.574.900,00",
+		rendimento: "1500,00",
+		extrato: "Em breve",
+		password: "abc123"
+	});
+	await BankAccount.create({
+		cod: "060",
+		bank: "Nubank",
+		account: "123456",
+		agency: "0001",
+		user_id: user.id
+	});
+	await BankAccount.create({
+		cod: "237",
+		bank: "Bradesco",
+		account: "123459",
+		agency: "1234",
+		user_id: user.id
+	});
 
-  const response = await client.get(`/bankaccount/${user.id}`).end();
+	const response = await client.get(`/bankaccount/${user.id}`).end();
 
-  response.assertStatus(200);
-  assert.exists(response.body.bankaccounts);
+	response.assertStatus(200);
+	assert.exists(response.body.bankaccounts);
 });
