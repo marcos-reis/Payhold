dff79020042ea76d23c5884ee69d67424253b5ea
Marcos Reis <42407618+marcos-reis@users.noreply.github.com>
2019-11-27 22:08:38 -0300

    feature dashboard client

--- web/src/Components/Client/Navbar/index.js
+++ web/src/Components/Client/Navbar/index.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
+import MaterialIcon from 'material-icons-react';
 import {Link} from 'react-router-dom'
-
-import User from '../../../Assets/marcos-reis.jpg';
-
 import {logout} from '../../../services/auth'
 
-// import { Container } from './styles';
+import './style.css';
 export default function Navbar() {
+
+ const [ main,setMain] = useState(true)
+  const openMain=()=>{
+setMain(!main)
+  }
+
+
+
   return (
-    <>
-      <nav className="navbar bg-light ">
-        <img className="ml-auto" style={{ width: '60px', borderRadius: '50%' }} src={User} alt="Marcos Reis" />
-
-        <div className="ml-3 ">
-          <h5 className="text-gray">Marcos Reis</h5>
-
-          <h6 className="text-grey font-weight-bold">Founder</h6>
-
-        </div>
-        <Link to='/dashboard' className="btn btn-outline-danger ml-5" onClick={()=>{logout()}}>sair</Link>
-
+    <div className="row">
+      <nav style={{zIndex:1}} className="position-fixed navbar bg-custom-primary col-12 ">
+     <div onClick={()=> openMain()} ><MaterialIcon size="50" color='#ffffff' icon="menu" /></div>
+     <h1 className=" text-light font-weight-bold text-center">Payhold</h1>
       </nav>
 
+    <div className={` position-fixed col-xl-2 col-lg-3 col-md-4 vh-100 justify-content-center flex-column d-md-flex d-none bg-light ${main?'custom-sidebar-on':'custom-sidebar-off ' }`}>
 
-    </>
+      <div className="d-flex flex-column h-75 justify-content-center px-5  ">
+
+      <Link to="/dashboard"><h5 className="text-dark ">Início</h5></Link>
+      <Link to="/extrato"><h5 className="text-dark ">Extrato</h5></Link>
+      <Link to="/conta" ><h5 className="text-dark ">Contas</h5></Link>
+      <Link to="/saque"> <h5 className="text-dark ">Retiradas</h5></Link>
+      </div>
+
+<Link to='/dashboard' style={{fontSize:20}} className="text-dark font-weight-bold text-center " onClick={()=>{logout()}}>sair</Link>
+</div>
+
+
+    </div>
   );
 }
