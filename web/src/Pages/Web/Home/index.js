67a4e4e492b00507ffdafa9c79c87ff89ddc2b62
Marcos Reis <42407618+marcos-reis@users.noreply.github.com>
2019-12-04 17:27:19 -0300

    Update index.js

--- web/src/Pages/Web/Home/index.js
+++ web/src/Pages/Web/Home/index.js
@@ -8,133 +8,140 @@ import apple from '../../../Assets/apple-play-store.png';
 import google from '../../../Assets/google-play-store.png';
 import shape from '../../../Assets/liquid-shape.png';
 import appMobile from '../../../Assets/iPhoneX.png';
+
 import { isNumber } from 'util';
 
 
 export default function Home() {
 
-  const [paymentFake, setPaymentFake] = useState('R$ 100,00');
-  const [cashBackFake, setCashBackFake] = useState('R$ 12,00');
-  const [percentCash, setPercentCash] = useState('12%');
+	const [payment, setPayment] = useState('R$ 100,00');
+	const [cashBack, setCashBack] = useState('R$ 12,00');
+	const [percentOFCash, setPercentOFCash] = useState('12%');
+
 
 const formatASMoney = (n) => {
-  const numberFormated = formatASNumber(n)
-  const moneyFormated = numberFormated.toLocaleString('pt-br',{style:'currency',currency:'BRL'})
-  setPaymentFake(moneyFormated)
-  returnCashBack(paymentFake,percentCash)
+	const numberFormated = formatASNumber(n)
+	const moneyFormated = numberFormated.toLocaleString('pt-br',{style:'currency',currency:'BRL'})
+	return moneyFormated
 }
 const formatASPercentage = (n) =>{
-  const numberFormated = formatASNumber(n)
-  const percentageFormated = numberFormated.toLocaleString('pt-br',{style:'percent'});
-  setPercentCash(percentageFormated)
-  returnCashBack(paymentFake,percentCash)
-}
-const returnCashBack = (v,i)=>{
-  const valueCashBackFormated = formatASNumber(v)
-  const taxaCashBack = formatASNumber(i)
-  const result = (valueCashBackFormated*taxaCashBack).toLocaleString('pt-br',{style:'currency',currency:'BRL'})
-  setCashBackFake(result)
+	const numberFormated = formatASNumber(n)
+	const percentageFormated = numberFormated.toLocaleString('pt-br',{style:'percent'});
+	return percentageFormated
 }
 const formatASNumber = (n)=> {
-  if(!isNumber(n)){return  parseInt(n.replace(/\D/g, ''))/100}
-  if(isNumber(n)){return  n/100}
+	if(!isNumber(n)){return  parseInt(n.replace(/\D/g, ''))/100}
+	if(isNumber(n)){return  n/100}
 
+}
+const changeState = (setState,State) =>{
+	setState(State)
+	calculateCashBack(payment,percentOFCash)
+}
+
+const calculateCashBack = (v,i)=>{
+	const valueCashBackFormated = formatASNumber(v)
+	const taxaCashBack = formatASNumber(i)
+	const result = (valueCashBackFormated*taxaCashBack).toLocaleString('pt-br',{style:'currency',currency:'BRL'})
+	setCashBack(result)
 }
 
 
-  return (
-    <>
-      <Navbar />
-      <div className="bg-primary">
-        <div className="container">
-          <div className="row pb-4">
-            <div className="col-xl-7 col-lg-7 col-sm pt-5 pb-5">
-              <h2 className="text-light font-weight-bold pt-5 ">
-          Ganhe cashback nas suas compras online.
-              </h2>
-              <h3 className="text-light font-weight-light">
-          Faça seu registro e baixe o nosso app.
-              </h3>
-              <img className="mt-5 " style={{ width: '200px' }} alt="Apple Store" src={apple} />
-              <img className="mt-5 " style={{ width: '200px' }} alt="Google Play Store" src={google} />
 
-            </div>
-            <div className="d-lg-block bg-shape w-25 d-none pb-5">
-              <img src={shape} className="" alt="none" />
-            </div>
-          </div>
-        </div>
+return (
+<>
 
-      </div>
-      <div className="container">
-        <article>
-          <h1 className="text-gray text-center mt-5 mb-5 display-4 font-weight-bold">Como funciona?</h1>
-          <h5 className="text-grey text-justify  mx-md-5 px-5 mb-5 pb-5 font-weight-normal">
-          Acesse nossa plataforma, crie uma conta,
-          escolha um parceiro e realize sua compra.
-          Cada parceiro ofertará um percentual de retorno
-          em nossa plataforma que pode variar por categorias de produtos.
-          O valor do cashback poderá ser sacado ou mantidos rendendo 100% do CDI.
+<Navbar />
+<div className="bg-primary">
+<div className="container">
+<div className="row pb-4">
+<div className="col-xl-7 col-lg-7 col-sm pt-5 pb-5">
+<h2 className="text-light font-weight-bold pt-5 ">
+Ganhe cashback nas suas compras online.
+</h2>
+<h3 className='text-light font-weight-light'>
+Faça seu registro e baixe o nosso app.
+</h3>
+<img className="mt-5 " style={{ width: '200px' }} alt="Apple Store" src={apple} />
+<img className="mt-5 " style={{ width: '200px' }} alt="Google Play Store" src={google} />
 
-          </h5>
-        </article>
-        <div className="row mb-5">
-          <div className="col-md col-12  mx-md-0 mx-sm-5 pl-5">
-            <MaterialIcon icon="touch_app" color="#2B6BCB" size="large" />
-            <p className="font-weight-light">Acesse nosso app e realize seu login</p>
-            <MaterialIcon icon="done" color="#2B6BCB" size="large" />
-            <p className="font-weight-light">Escolha o parceiro que está em nossa lista</p>
-            <MaterialIcon icon="shopping_cart" color="#2B6BCB" size="large" />
-            <p className="font-weight-light">Escolha o produto que deseja e realize sua compra</p>
-            <MaterialIcon icon="monetization_on" color="#2B6BCB" size="large" />
-            <p className="font-weight-light">Receba o cashback</p>
-          </div>
-          <div className="col text-center">
+</div>
+<div className="d-lg-block bg-shape w-25 d-none pb-5">
+<img src={shape} className="" alt="none" />
+</div>
+</div>
+</div>
+
+</div>
+<div className="container">
+<article>
+<h1 className="text-gray text-center mt-5 mb-5 display-4 font-weight-bold">Como funciona?</h1>
+<h5 className="text-grey text-justify  mx-md-5 px-5 mb-5 pb-5 font-weight-normal">
+	Acesse nossa plataforma, crie uma conta,
+	escolha um parceiro e realize sua compra.
+	Cada parceiro ofertará um percentual de retorno
+	em nossa plataforma que pode variar por categorias de produtos.
+	O valor do cashback poderá ser sacado ou mantidos rendendo 100% do CDI.
+
+</h5>
+</article>
+<div className="row mb-5">
+<div className="col-md col-12  mx-md-0 mx-sm-5 pl-5">
+<MaterialIcon icon="touch_app" color="#2B6BCB" size="large" />
+<p className="font-weight-light">Acesse nosso app e realize seu login</p>
+<MaterialIcon icon="done" color="#2B6BCB" size="large" />
+<p className="font-weight-light">Escolha o parceiro que está em nossa lista</p>
+<MaterialIcon icon="shopping_cart" color="#2B6BCB" size="large" />
+<p className="font-weight-light">Escolha o produto que deseja e realize sua compra</p>
+<MaterialIcon icon="monetization_on" color="#2B6BCB" size="large" />
+<p className="font-weight-light">Receba o cashback</p>
+</div>
+<div className="col text-center">
 
 
-            <img src={appMobile} style={{width:'250px'}} alt="App Payhold" />
+<img src={appMobile} style={{width:'250px'}} alt="App Payhold" />
 
-          </div>
-        </div>
-      </div>
+</div>
+</div>
+</div>
 
-      <div className="py-5" style={{ backgroundColor: '#3C86F6' }}>
-        <div className="container">
-          <div className="row">
-            <div className=" col-md-8 col-lg-7 col-sm-10 col-10 bg-white text-center mx-auto px-sm-5 py-5">
-              <h3 className=" text-justify px-sm-5 font-weight-bold text-gray"> Veja o total de CashBack concedido em tempo real!</h3>
-              <h6 className=" text-justify px-sm-5 mb-4 font-weight-light">Cashback que retorna para você melhor investir.</h6>
-              <div className="border border-primary w-100 mx-auto mb-5">
+<div className="py-5" style={{ backgroundColor: '#3C86F6' }}>
+<div className="container">
+<div className="row">
+<div className=" col-md-8 col-lg-7 col-sm-10 col-10 bg-white text-center mx-auto px-sm-5 py-5">
+<h3 className=" text-justify px-sm-5 font-weight-bold text-gray"> Veja o total de CashBack concedido em tempo real!</h3>
+<h6 className=" text-justify px-sm-5 mb-4 font-weight-light">Cashback que retorna para você melhor investir.</h6>
+<div className="border border-primary w-100 mx-auto mb-5">
 
-                <p className="text-center my-3">
+<p className="text-center my-3">
 
-                  <input
-                        onChange={(e) => formatASMoney(e.target.value)}
-                        className="font-weight-bold text-grey ml-3 w-50 border-0"
-                        onBlur={() => formatASMoney(paymentFake)}
-                        style={{ fontSize: 40 }} value={paymentFake} />
+<input
+onChange={/*(e) => formatASMoney(e.target.value)*/ (e)=>  changeState(setPayment,formatASMoney(e.target.value))}
+className="font-weight-bold text-grey ml-3 w-50 border-0"
+onBlur={/*() => formatASMoney(payment)*/ ()=>  changeState(setPayment,formatASMoney(payment))}
+style={{ fontSize: 40 }} value={payment} />
 
-                </p>
-              </div>
+</p>
+</div>
 <div className=" text-center ">
-              <h5 className="font-weight-bold  mt-4  text-center">
+<h5 className="font-weight-bold  mt-4  text-center">
 Cash:
-                <input onChange={(e) => setPercentCash(e.target.value)} className="font-weight-bold text-grey w-25 border-0" onBlur={() => formatASPercentage(percentCash)} value={percentCash}/>
-              </h5>
+<input onChange={/*(e) => setPercentOFCash(e.target.value)*/(e)=>changeState(setPercentOFCash,formatASPercentage(e.target.value))} className="font-weight-bold text-grey w-25 border-0" onBlur={/*() => formatASPercentage(percentOFCash)*/ ()=>changeState(setPercentOFCash,formatASPercentage(percentOFCash))} value={percentOFCash}/>
+</h5>
 
-              <h5 className="font-weight-bold ">
+<h5 className="font-weight-bold ">
 Retorno:
 
-                <span className="font-weight-bold ml-2 text-primary">{cashBackFake}</span>
-              </h5>
-              </div>
-            </div>
-          </div>
+<span className="font-weight-bold ml-2 text-primary">{cashBack}</span>
+</h5>
+</div>
+</div>
+</div>
 
-        </div>
+</div>
 
-      </div>
-      <Footer />
-    </>
-  );
+</div>
+<Footer />
+</>
+);
 }
